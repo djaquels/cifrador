fed72d6a413c8be02353a36cfd7e6e5751b69a57
djaquels <hjacales@gmail.com>
2021-01-11 01:45:32 -0600

    data for filesystem

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
+# Bibliotecas usadas por el proyecto
+# Flask bilbioteca WEB
 from flask import Flask
 from flask import Flask, render_template, redirect, url_for
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+# Crypto es usada para las funciones de seguridad
 from Crypto.PublicKey import RSA
 from Crypto import Random
 from Crypto.Cipher import PKCS1_OAEP
 import pem
+# Para acceso al sistema de archivos
 import os
-
+# Biblioteca para la Base de Datos
 from database.repositorio import Repositorio
 
 app = Flask(__name__)
+# Permite conexión segura desde fuentes confiables
 CORS(app)
 
+#Metodo auxiliar para saber si un usuario existe en los registros.
 def existe_usuario(user):
     rep = Repositorio("postgres", "postgres", "127.0.0.1", "5432", "cypher")
     query = "SELECT count(*) FROM users where username = %s "
@@ -24,19 +30,23 @@ def existe_usuario(user):
         return False
     else:
         return True
-
+# Metodo auxiliar para obtener el total de de usuarios registrados.
 def get_usuarios():
     rep = Repositorio("postgres", "postgres", "127.0.0.1", "5432", "cypher")
     query = "SELECT count(*) FROM users "
     rep.cursor.execute(query)
     total = rep.cursor.fetchone()[0]
     return total
+
+# Metodo index, pantalla principal
 @app.route('/',methods=['POST','GET','OPTIONS'])
 def index():
     total = get_usuarios()
     return render_template('index.html',total=total)
+# Metodo encargado de desplegar la lista de usuarios y sus llaves publicas.    
 @app.route('/users',methods=['GET','OPTIONS'])
 def users_list():
+    total = get_usuarios()
     try:
         print("reading users table")
         rep = Repositorio("postgres", "postgres", "127.0.0.1", "5432", "cypher")
@@ -44,10 +54,12 @@ def users_list():
         rep.cursor.execute(query)
         data = [ x for x in rep.cursor]
         print(data)
-        return render_template('users.html',data=data)
+        return render_template('users.html',data=data,total=total)
     except  Exception as e:
         print("error papu")
-        return  render_template('users.html',data={})
+        return  render_template('users.html',data={},total=total)
+
+ # Metodo que se encarga de registrar usuarios de forma segura.       
 @app.route('/generated',methods=['POST','GET','OPTIONS'])
 def generate_keys():
     total = get_usuarios()
@@ -82,6 +94,8 @@ def generate_keys():
         os.remove("./private_key.rsa") 
         os.remove("./public_key.rsa") 
         return render_template('generated.html',private=pk,public=public,passphrase=encrypted_text,total=total)
+
+# Metodo que apartir de un archivo y el nombre usuario valida la identidad del usuario.
 @app.route('/validate',methods=['POST','GET','OPTIONS'])
 def validate():
     status = False
@@ -107,6 +121,7 @@ def validate():
             pk = open("./storage/{}".format(filename),"rb")
             rsa_private_key = RSA.importKey(pk.read())
             print("key imported")
+            pk.close()
             rsa_private_key = PKCS1_OAEP.new(rsa_private_key)
             decrypted_text = rsa_private_key.decrypt(passphrase)
             print('your decrypted_text is : {}'.format(decrypted_text))
@@ -115,7 +130,10 @@ def validate():
         except  Exception as e:
             status = False
             mensaje = "No coinciden las llaves, no se valido el usuario."
+        os.remove("./storage/{}".format(filename)) 
         return render_template('validated.html',status=status,mensaje=mensaje,total=total)
     return render_template('validate.html',total=total)
+
+# Inicializa el servidor WEB para la aplicación.    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8057)
--- database/repositorio.py
+++ database/repositorio.py
@@ -1,11 +1,13 @@
 # Conexion y consultas a la base de datos
+# Modulo para hacer consultas a PostgreSQL
 import psycopg2
 
-
+# Clase auxiliar para acceder a las funcionalidades de PostgreSQL
 class Repositorio:
+    # Estos atributos son los que guardan el estado de la conexion
     connection = None
     cursor = None
-
+    # Al iniciar un objeto se conecta a la base de datos
     def __init__(self, user, password, host, port, database):
         self.user = user
         self.password = password
@@ -13,6 +15,7 @@ class Repositorio:
         self.port = port
         self.database = database
         try:
+            # recibe como parametros los datos de conexión
             self.connection = psycopg2.connect(user=self.user,
                                                 password=self.password,
                                                 host=self.host,
@@ -28,7 +31,7 @@ class Repositorio:
             print("You are connected to - ", record, "\n")
         except (Exception, psycopg2.Error) as error:
             print("Error while connecting to PostgreSQL", error)
-
+    # Metodo para cerrar el socket de conexión y no dejar conexiones abiertas
     def disconnect(self):
         self.cursor.close()
         self.connection.close()
